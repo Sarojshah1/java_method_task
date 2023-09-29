784850e4ccc24e95fc3b635fd917a86674c1dc4f
saroj kumar sah <sarojahah152@gmail.com>
2023-09-29 09:04:09 +0545

    javaMethod4

--- JavaMethod4.java
+++ JavaMethod4.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
 public class JavaMethod4 {
+    // 4. Write a program for multiplying 3 numbers using a method and return the result and print the result from the main method.
+    static int multiplying(int a, int b, int c){
+        return a*b*c;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter first number");
+        int a=scanner.nextInt();
+        System.out.println("enter second number");
+        int b=scanner.nextInt();
+        System.out.println("enter third number");
+        int c=scanner.nextInt();
+        int d=multiplying(a, b, c);
+        System.out.println("the multiplication of " + a + "," + b + " and " + c + " is "+d);
+        
+        scanner.close();
+    }
     
 }
