3903d75171bcfb3875c46eee3f82cb30ef834ca5
saroj kumar sah <sarojahah152@gmail.com>
2023-09-29 09:20:38 +0545

    javaMethod7

--- JavaMethod7.java
+++ JavaMethod7.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+
 public class JavaMethod7 {
+    // 7. Calculates the factorial of a given number using the Java method.
+    static int factorial(int number) {
+        int result =1;
+       for (int i = 1; i <= number; i++) {
+        result = result*i;
+        
+       }
+       return result;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter a number for factorial");
+        int a=scanner.nextInt();
+        System.out.println("The factorial of "+a+" is "+factorial(a));
+        scanner.close();
+    }
     
 }
