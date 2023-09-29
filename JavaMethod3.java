dc15888afebcb133d53afcbb0f52490552ffe284
saroj kumar sah <sarojahah152@gmail.com>
2023-09-29 08:59:27 +0545

    javaMethod3

--- JavaMethod3.java
+++ JavaMethod3.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+
 public class JavaMethod3 {
+    // 3. Make a method to print the table of a given number n.
+    static void table(int n){
+        for(int i=1; i<=n; i++){
+            System.out.println("********************************");
+            for(int j=1; j<=10; j++){
+                System.out.println(i+" X "+j+" = "+i*j);
+    }
+}
     
+}
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+        System.out.println("enter a number");
+        int a=scanner.nextInt();
+        table(a);
+        scanner.close();
+}
+
 }
