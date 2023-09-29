54ea69a06931783182f1bdc0fd63cd1f4d0e81c4
saroj kumar sah <sarojahah152@gmail.com>
2023-09-29 08:53:05 +0545

    javaMethod2

--- JavaMethod10.java
+++ JavaMethod10.java
@@ -1,4 +1,21 @@
+import java.util.Scanner;
+
 public class JavaMethod10 {
     // 2. Make a method to check if a given number n is even or not.
+    static String check(int n) {
+        if(n%2==0){
+            return n+" is even";
+        }
+        else{
+            return n+" is not even";
+        }
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter a number");
+        int a=scanner.nextInt();
+        System.out.println(check(a));
+        scanner.close();
+    }
     
 }
