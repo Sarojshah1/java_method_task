a934b81b1c36a14c7c01d562f222b60103264491
saroj kumar sah <sarojahah152@gmail.com>
2023-09-29 09:09:39 +0545

    javaMethod5

--- JavaMethod5.java
+++ JavaMethod5.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class JavaMethod5 {
+    // 5. Write a program for calculating Simple Interest using a method and print the result from the method itself.
+    static void Simple(int a,int b,int c){
+        System.out.println("The simple interest is "+(a*b*c)/100);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter principal");
+        int a=scanner.nextInt();
+        System.out.println("enter time(in years)");
+        int b=scanner.nextInt();
+        System.out.println("enter rate of interest");
+        int c=scanner.nextInt();
+        Simple(a, b, c);
+        scanner.close();
+    }
     
 }
