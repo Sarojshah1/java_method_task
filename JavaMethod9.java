2d495b09d31f755acc4523e8ab22f7682622ae26
saroj kumar sah <sarojahah152@gmail.com>
2023-09-29 09:28:20 +0545

    javaMethod9

--- JavaMethod9.java
+++ JavaMethod9.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
+
 public class JavaMethod9 {
+    // 9. Write a Java program to find the greatest common divisor (GCD) of two numbers.
+    public static int findGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt();
+        
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt();
+        
+        int gcd = findGCD(num1, num2);
+        
+        System.out.println("The GCD of " + num1 + " and " + num2 + " is: " + gcd);
+        
+        scanner.close();
+    }
     
 }
